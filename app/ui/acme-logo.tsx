a0c0a2bf652d0405af9cb87c534dc45ef8903bb4
ThiagoCalisto <warlocalgames@gmail.com>
2024-02-04 21:22:09 -0300

    coloquei a barra de pesquisa, mas nao ajustei

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,11 +1,17 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import { Gabarito } from 'next/font/google';
 
+  const gabarito = Gabarito({
+      subsets: ['latin'],
+      display: 'swap',
+    })
 export default function AcmeLogo() {
+
+  
   return (
     <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
-    style={{ marginRight:'auto'}}>
+      className={`${gabarito.className} flex flex-row items-center leading-none text-white`}
+    >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]" style={{color: '#3D00B7'}}>NtemNome</p>
     </div>
--- app/ui/dashboard/footer.tsx
+++ app/ui/dashboard/footer.tsx
@@ -1,17 +1,33 @@
 import AcmeLogo from '@/app/ui/acme-logo';
+import { DM_Sans } from 'next/font/google';
+import SearchImage from '../../../src/assets/images/search-normal.png';
 
-export default function Footer(){
-  return(
-        <div className="flex h-12 shrink-0 items-end p-4 md:h-20" style={{backgroundColor: '#fffff', borderBottom: '1px solid rgba(0, 0, 0, 0.5)'}}>
-           <AcmeLogo />
-            <nav className="ml-4" style={{left:'500px'}}>
-                <ul className="flex gap-4">
-                  <li>Opção 1</li>
-                  <li>Opção 2</li>
-                  <li>Opção 3</li>
-                </ul>
-              </nav>
-                <button>Botão</button>
-        </div>
+const dm = DM_Sans({
+  subsets: ['latin-ext'],
+  weight: ['400', '1000'],
+  style: ['normal'],
+})
+
+export default function Footer() {
+  return (
+    <div className="flex h-12 shrink-0 items-end p-4 md:h-20" style={{ backgroundColor: '#fffff', borderBottom: '1px solid rgba(0, 0, 0, 0.1)' }}>
+      <AcmeLogo />
+      <div className="h-full mx-4" style={{ borderRight: '1px solid rgba(0, 0, 0, 0.1)', marginLeft: '36px', marginRight: '36px' }}></div>
+      <nav className={`${dm.className}`} style={{ marginBottom: '7px' }}>
+        <ul className={`${dm.className} flex gap-4`}>
+          <li className='font-bold'>Menu</li>
+          <li className='ml-10 font-bold'>Combos</li>
+          <li className='ml-10 font-bold'>Sobre</li>
+        </ul>
+      </nav>
+      <div className="ml-4 mb-1 border rounded-full px-6 py-1" style={{ display: 'flex', alignItems: 'center' }}>
+        <input
+          type="text"
+          placeholder="Pesquisar Item"
+          style={{ width: '200px', border: 'none' }}
+        />
+        <img src={SearchImage.src} alt="Search Icon" style={{ width: '23px', height: '23px' }} />
+      </div>
+    </div>
   )
 }
